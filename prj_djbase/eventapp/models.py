e665aefa6a59712e066de531de127629f6804ae3
Nik A Zeyn <anar.zeynalov@hotmail.com>
2020-07-12 04:09:34 +1000

    updated: models slight changes

--- prj_djbase/eventapp/models.py
+++ prj_djbase/eventapp/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class SkillGrid(models.Model):
+class Category(models.Model):
 	name = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.name
 
-class Scope(models.Model):
+class Type(models.Model):
 	name = models.CharField(max_length=200)
-	skillgrid = models.ForeignKey(SkillGrid, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name
@@ -19,7 +19,7 @@ class Scope(models.Model):
 class Event(models.Model):
 	user = models.ForeignKey(User, on_delete=models.PROTECT)
 	title = models.CharField(max_length=200)
-	scope = models.ForeignKey(Scope, on_delete=models.PROTECT)
+	type = models.ForeignKey(Type, on_delete=models.PROTECT)
 
 	def __str__(self):
 		return self.title
