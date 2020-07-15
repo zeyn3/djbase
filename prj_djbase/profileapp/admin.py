28ef59843d0595c3fb91ad7bd0f15d686ff3cd39
Nik A Zeyn <anar.zeynalov@hotmail.com>
2020-07-15 13:59:01 +1000

    added: vendor certs v2

--- prj_djbase/profileapp/admin.py
+++ prj_djbase/profileapp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from profileapp.models import Vendor, Category, VendorCategory
+# 
 # Register your models here.
 
+# 1:M Setup
+from profileapp.models import Vendor, Category, Certification, VendorCertification
 admin.site.register(Vendor)
 admin.site.register(Category)
-admin.site.register(VendorCategory)
+admin.site.register(Certification)
+admin.site.register(VendorCertification)
 
 
-
+# M2m Relations
+# from profileapp.models import Vendor, Category, VendorCategory
+# admin.site.register(Vendor)
+# admin.site.register(Category)
+# admin.site.register(VendorCategory)
--- prj_djbase/profileapp/models.py
+++ prj_djbase/profileapp/models.py
@@ -3,6 +3,58 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Vendor(models.Model):
+	name = models.CharField(max_length=200)	
+	
+	class Meta:
+		verbose_name_plural = "vendors"
+
+	def __str__(self):
+		return self.name
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = "categories"
+
+	def __str__(self):
+		return self.name
+
+
+class Certification(models.Model):
+	title = models.CharField(max_length=200)
+	vendor = models.ForeignKey(Vendor, related_name='vendors',
+					default=3, on_delete=models.SET_DEFAULT)
+	category = models.ForeignKey(Category, related_name='categories',
+					default=3, on_delete=models.SET_DEFAULT)
+
+
+	class Meta:
+		verbose_name_plural = "certifications"
+
+	def __str__(self):
+		return self.title
+
+
+class VendorCertification(models.Model):
+	certification = models.ForeignKey(Certification, related_name='vendor_certs',
+					default=3, on_delete=models.SET_DEFAULT)
+	user = models.ForeignKey(User, related_name='vendor_certs',
+					on_delete=models.CASCADE)
+	issue_date = models.DateField()
+	expiry_date = models.DateField()
+
+
+	class Meta:
+		verbose_name_plural = "vendor certifications"
+
+	def __str__(self):
+		if self.user.first_name or self.user.last_name:
+			return f"{self.user.first_name} {self.user.last_name} - {self.certification.title}" 
+		else:
+			return f"User {self.user.id} - {self.certification.title}" 
 
 
 
